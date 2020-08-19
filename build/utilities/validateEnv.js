4d42a91c2fcb88e4aec6ff562f53496714914431
Krzysztof Surażyński <krzychna33@gmail.com>
2020-08-19 11:29:47 +0200

    Deleted build dir

--- build/utilities/validateEnv.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateEnv = void 0;
-var envalid_1 = require("envalid");
-exports.validateEnv = function () {
-    envalid_1.cleanEnv(process.env, {
-        PORT: envalid_1.port(),
-    });
-};
